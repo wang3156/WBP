95435c3e51dbfa6539661b2b5bd7bc741242ea2b
Wang3156 <wangming135@qq.com>
2020-07-08 11:06:36 +0800

    添加js  将table导成excel (已解决中文乱码)

--- Pone/WebFramework1/Scripts/jsObjectExtend.js
+++ Pone/WebFramework1/Scripts/jsObjectExtend.js
@@ -138,4 +138,29 @@
             }
         });
     }
+
+    //将页面上的table导成一个excel
+    $.tableExportXls = function (table, name) {
+        var uri = 'data:application/vnd.ms-excel;base64,',
+            template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head> <meta http-equiv=\'Content-Type\' content=\'application/vnd.ms-excel; charset=utf-8\' /><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table border="1">{table}</table></body></html>',
+            base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) },
+            format = function (s, c) {
+                return s.replace(/{(\w+)}/g,
+                        function (m, p) { return c[p]; })
+            }
+        if (!table.nodeType) table = document.getElementById(table)
+
+        var trs = $(table).find("tr");
+        if (trs.length > 5) {
+            trs.remove(':gt(' + (trs.length - 6) + ')')
+        }
+        var ctx = { worksheet: 'sheet1', table: table.innerHTML }
+        var a = document.createElement("a");
+        a.download = name;
+        a.href = uri + base64(format(template, ctx));
+        a.target = "_blank";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
 })();
