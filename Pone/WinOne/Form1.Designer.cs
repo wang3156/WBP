a87afbd3461a28de980bb3e96330fc54e5e6c37e
Wang3156 <wangming135@qq.com>
2020-08-19 14:06:51 +0800

    添加文字对比的方法

---
+++ Pone/WinOne/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WinOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.rtb2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtb1
+            // 
+            this.rtb1.Location = new System.Drawing.Point(12, 39);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(634, 692);
+            this.rtb1.TabIndex = 0;
+            this.rtb1.Text = "";
+            // 
+            // rtb2
+            // 
+            this.rtb2.Location = new System.Drawing.Point(668, 39);
+            this.rtb2.Name = "rtb2";
+            this.rtb2.Size = new System.Drawing.Size(622, 692);
+            this.rtb2.TabIndex = 0;
+            this.rtb2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(799, 747);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "对比";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1302, 823);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtb2);
+            this.Controls.Add(this.rtb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.RichTextBox rtb2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
