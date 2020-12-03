eab25d792f77ebf28eba0a845bff29650a516ffe
Wang3156 <wangming135@qq.com>
2020-12-03 17:18:36 +0800

    添加购物车等内容

--- Pone/ShoppingPro2/Scripts/pbl/js/main.js
+++ Pone/ShoppingPro2/Scripts/pbl/js/main.js
@@ -8,44 +8,44 @@
  * Copyright 2015, Codrops
  * http://www.codrops.com
  */
-;(function(window) {
+; (function (window) {
 
-	'use strict';
+    'use strict';
 
-	var support = { animations : Modernizr.cssanimations },
-		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
-		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
-		onEndAnimation = function( el, callback ) {
-			var onEndCallbackFn = function( ev ) {
-				if( support.animations ) {
-					if( ev.target != this ) return;
-					this.removeEventListener( animEndEventName, onEndCallbackFn );
-				}
-				if( callback && typeof callback === 'function' ) { callback.call(); }
-			};
-			if( support.animations ) {
-				el.addEventListener( animEndEventName, onEndCallbackFn );
-			}
-			else {
-				onEndCallbackFn();
-			}
+    var support = { animations: Modernizr.cssanimations },
+		animEndEventNames = { 'WebkitAnimation': 'webkitAnimationEnd', 'OAnimation': 'oAnimationEnd', 'msAnimation': 'MSAnimationEnd', 'animation': 'animationend' },
+		animEndEventName = animEndEventNames[Modernizr.prefixed('animation')],
+		onEndAnimation = function (el, callback) {
+		    var onEndCallbackFn = function (ev) {
+		        if (support.animations) {
+		            if (ev.target != this) return;
+		            this.removeEventListener(animEndEventName, onEndCallbackFn);
+		        }
+		        if (callback && typeof callback === 'function') { callback.call(); }
+		    };
+		    if (support.animations) {
+		        el.addEventListener(animEndEventName, onEndCallbackFn);
+		    }
+		    else {
+		        onEndCallbackFn();
+		    }
 		};
 
-	// from http://www.sberry.me/articles/javascript-event-throttling-debouncing
-	function throttle(fn, delay) {
-		var allowSample = true;
+    // from http://www.sberry.me/articles/javascript-event-throttling-debouncing
+    function throttle(fn, delay) {
+        var allowSample = true;
 
-		return function(e) {
-			if (allowSample) {
-				allowSample = false;
-				setTimeout(function() { allowSample = true; }, delay);
-				fn(e);
-			}
-		};
-	}
+        return function (e) {
+            if (allowSample) {
+                allowSample = false;
+                setTimeout(function () { allowSample = true; }, delay);
+                fn(e);
+            }
+        };
+    }
 
-	// sliders - flickity
-	var sliders = [].slice.call(document.querySelectorAll('.slider')),
+    // sliders - flickity
+    var sliders = [].slice.call(document.querySelectorAll('.slider')),
 		// array where the flickity instances are going to be stored
 		flkties = [],
 		// grid element
@@ -53,89 +53,95 @@
 		// isotope instance
 		iso,
 		// filter ctrls
-		filterCtrls = [].slice.call(document.querySelectorAll('.filter > button'));
-    //,
-	//	// cart
-	//	cart = document.querySelector('.cart'),
-	//	cartItems = cart.querySelector('.cart__count');
+		filterCtrls = [].slice.call(document.querySelectorAll('.filter > button'))
+       ,
+		// cart
+		cart = document.querySelector('.cart'),
+		cartItems = cart.querySelector('.cart__count');
 
-	function init() {
-		// preload images
-		imagesLoaded(grid, function() {
-			initFlickity();
-			initIsotope();
-			initEvents();
-			classie.remove(grid, 'grid--loading');
-		});
-	}
+    function init() {
+        // preload images
+        imagesLoaded(grid, function () {
+            initFlickity();
+            initIsotope();
+            initEvents();
+            classie.remove(grid, 'grid--loading');
+        });
+    }
 
-	function initFlickity() {
-		sliders.forEach(function(slider){
-			var flkty = new Flickity(slider, {
-				prevNextButtons: false,
-				wrapAround: true,
-				cellAlign: 'left',
-				contain: true,
-				resize: false
-			});
+    function initFlickity() {
+        sliders.forEach(function (slider) {
+            var flkty = new Flickity(slider, {
+                prevNextButtons: false,
+                wrapAround: true,
+                cellAlign: 'left',
+                contain: true,
+                resize: false
+            });
 
-			// store flickity instances
-			flkties.push(flkty);
-		});
-	}
+            // store flickity instances
+            flkties.push(flkty);
+        });
+    }
 
-	function initIsotope() {
-		iso = new Isotope( grid, {
-			isResizeBound: false,
-			itemSelector: '.grid__item',
-			percentPosition: true,
-			masonry: {
-				// use outer width of grid-sizer for columnWidth
-				columnWidth: '.grid__sizer'
-			},
-			transitionDuration: '0.6s'
-		});
-	}
+    function initIsotope() {
+        iso = new Isotope(grid, {
+            isResizeBound: false,
+            itemSelector: '.grid__item',
+            percentPosition: true,
+            masonry: {
+                // use outer width of grid-sizer for columnWidth
+                columnWidth: '.grid__sizer'
+            },
+            transitionDuration: '0.6s'
+        });
+    }
 
-	function initEvents() {
-		filterCtrls.forEach(function(filterCtrl) {
-			filterCtrl.addEventListener('click', function() {
-				classie.remove(filterCtrl.parentNode.querySelector('.filter__item--selected'), 'filter__item--selected');
-				classie.add(filterCtrl, 'filter__item--selected');
-				iso.arrange({
-					filter: filterCtrl.getAttribute('data-filter')
-				});
-				recalcFlickities();
-				iso.layout();
-			});
-		});
+    function initEvents() {
+        filterCtrls.forEach(function (filterCtrl) {
+            filterCtrl.addEventListener('click', function () {
+                classie.remove(filterCtrl.parentNode.querySelector('.filter__item--selected'), 'filter__item--selected');
+                classie.add(filterCtrl, 'filter__item--selected');
+                iso.arrange({
+                    filter: filterCtrl.getAttribute('data-filter')
+                });
+                recalcFlickities();
+                iso.layout();
+            });
+        });
 
-		// window resize / recalculate sizes for both flickity and isotope/masonry layouts
-		window.addEventListener('resize', throttle(function(ev) {
-			recalcFlickities()
-			iso.layout();
-		}, 50));
+        // window resize / recalculate sizes for both flickity and isotope/masonry layouts
+        window.addEventListener('resize', throttle(function (ev) {
+            recalcFlickities()
+            iso.layout();
+        }, 50));
 
-		// add to cart
-		[].slice.call(grid.querySelectorAll('.grid__item')).forEach(function(item) {
-			item.querySelector('.action--buy').addEventListener('click', addToCart);
-		});
-	}
+        // add to cart
+        [].slice.call(grid.querySelectorAll('.grid__item')).forEach(function (item) {
+            item.querySelector('.action--buy').addEventListener('click', addToCart);
+        });
+    }
 
-	function addToCart() {
-		classie.add(cart, 'cart--animate');
-		setTimeout(function() {cartItems.innerHTML = Number(cartItems.innerHTML) + 1;}, 200);
-		onEndAnimation(cartItems, function() {
-			classie.remove(cart, 'cart--animate');
-		});
-	}
+    function addToCart(e) {
+        var tar = e.target || e.currentTarget;
+        if (addToCartEvent) {
+            if (!addToCartEvent(tar)) {
+                return false;
+            }
+        }
+        classie.add(cart, 'cart--animate');
+        setTimeout(function () { cartItems.innerHTML = Number(cartItems.innerHTML) + 1; }, 200);
+        onEndAnimation(cartItems, function () {
+            classie.remove(cart, 'cart--animate');
+        });
+    }
 
-	function recalcFlickities() {
-		for(var i = 0, len = flkties.length; i < len; ++i) {
-			flkties[i].resize();
-		}
-	}
+    function recalcFlickities() {
+        for (var i = 0, len = flkties.length; i < len; ++i) {
+            flkties[i].resize();
+        }
+    }
 
-	init();
+    init();
 
 })(window);
