9ad2968383b73019dc4b874e9bf083547ba65980
Wang3156 <wangming135@qq.com>
2021-03-08 15:35:02 +0800

    添加array导出excel的功能

--- Pone/CommLibrary/js/jsObjectExtend.js
+++ Pone/CommLibrary/js/jsObjectExtend.js
@@ -84,7 +84,10 @@
 
 })();
 
-//Jquery
+//Jquery  
+//自定义post请求
+//将fie控件选择的文件转成base64
+//从base64转成文件保存
 (function () {
     //beforeSend 返回false则中断请求
     //error 返回true 表示已经在方法里处理了异常.返回false表示没有处理
@@ -259,7 +262,7 @@
 })();
 
 
-//数值
+//数值 加减剩除
 
 (function () {
     /**
@@ -391,4 +394,79 @@
     Number.prototype.div = function (arg) {
         return accDiv(this, arg);
     };
+})();
+
+
+//window 扩展
+/*
+//安装  xlsx.core.min.js 后使用
+利用 excel.js 将数组导成excel 
+*/
+(function () {
+    window.xlsxex = (function () {
+        var ex = {
+            LoadJS: function (src) {
+                var newscript = document.createElement('script');
+                newscript.setAttribute('type', 'text/javascript');
+                newscript.setAttribute('src', src);
+                var head = document.getElementsByTagName('head')[0];
+                head.appendChild(newscript);
+            },
+            // 将workbook装化成blob对象
+            workbook2blob: function (workbook) {
+                // 生成excel的配置项
+                var wopts = {
+                    // 要生成的文件类型
+                    bookType: "xlsx",
+                    // // 是否生成Shared String Table，官方解释是，如果开启生成速度会下降，但在低版本IOS设备上有更好的兼容性
+                    bookSST: false,
+                    type: "binary"
+                };
+                var wbout = XLSX.write(workbook, wopts);
+                // 将字符串转ArrayBuffer
+                function s2ab(s) {
+                    var buf = new ArrayBuffer(s.length);
+                    var view = new Uint8Array(buf);
+                    for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xff;
+                    return buf;
+                }
+                var blob = new Blob([s2ab(wbout)], {
+                    type: "application/octet-stream"
+                });
+                return blob;
+            },
+            // 将blob对象创建bloburl，然后用a标签实现弹出下载框
+            openDownloadDialog: function (blob, fileName) {
+                if (typeof blob == "object" && blob instanceof Blob) {
+                    blob = URL.createObjectURL(blob); // 创建blob地址
+                }
+                var aLink = document.createElement("a");
+                aLink.href = blob;
+                // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，有时候 file:///模式下不会生效
+                aLink.download = fileName || "";
+                var event;
+                if (window.MouseEvent) event = new MouseEvent("click");
+                    //   移动端
+                else {
+                    event = document.createEvent("MouseEvents");
+                    event.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                }
+                aLink.dispatchEvent(event);
+            },
+            //sheets:{sheet1name:[{}]}
+            downXlsxFileByArray: function (sheets, name) {
+                var wb = XLSX.utils.book_new();
+                for (var i in sheets) {
+                    XLSX.utils.book_append_sheet(wb, XLSX.utils.json_to_sheet(sheets[i]), i);
+                }
+                var workbookBlob = this.workbook2blob(wb);
+                this.openDownloadDialog(workbookBlob, name);
+            }
+
+        }
+        var spt = document.getElementsByTagName("script");
+        ex.LoadJS(spt[spt.length - 1].src.replace("xlsx.ex.js", "") + "xlsx.core.min.js");
+        return ex;
+    })();
+
 })();
