a39bc3c80c9cb18f25d6149fd234ca87b8534aef
Wang3156 <wangming135@qq.com>
2021-07-22 14:37:42 +0800

    添加字符字串符的扩展方法.使用GZIP对字符串进行压缩

--- Pone/CommLibrary/Extension/StringExtension.cs
+++ Pone/CommLibrary/Extension/StringExtension.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -48,6 +50,92 @@ namespace CommLibrary.Extension
                 word = word.ToUpper();
             }
             return str.Contains(word);
+        }
+
+        /// <summary>
+        /// 使用GZip压缩字符串 返回一个压缩后的base64字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>压缩后的base64字符串</returns>
+        public static string CompressString(this string str)
+        {
+            var compressBeforeByte = Encoding.GetEncoding("UTF-8").GetBytes(str);
+            var compressAfterByte = Compress(compressBeforeByte);
+            string compressString = Convert.ToBase64String(compressAfterByte);
+            return compressString;
+        }
+        /// <summary>
+        /// 解压以上面方式压缩过的字符串
+        /// </summary>
+        /// <param name="str">压缩生成的base64的字符串</param>
+        /// <returns></returns>
+        public static string DecompressString(this string str)
+        {
+            var compressBeforeByte = Convert.FromBase64String(str);
+            var compressAfterByte = Decompress(compressBeforeByte);
+            string compressString = Encoding.GetEncoding("UTF-8").GetString(compressAfterByte);
+            return compressString;
+        }
+
+        /// <summary>
+        /// Compress
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private static byte[] Compress(byte[] data)
+        {
+            try
+            {
+                var ms = new MemoryStream();
+                var zip = new GZipStream(ms, CompressionMode.Compress, true);
+                zip.Write(data, 0, data.Length);
+                zip.Close();
+                var buffer = new byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(buffer, 0, buffer.Length);
+                ms.Close();
+                return buffer;
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Decompress
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private static byte[] Decompress(byte[] data)
+        {
+            try
+            {
+                var ms = new MemoryStream(data);
+                var zip = new GZipStream(ms, CompressionMode.Decompress, true);
+                var msreader = new MemoryStream();
+                var buffer = new byte[0x1000];
+                while (true)
+                {
+                    var reader = zip.Read(buffer, 0, buffer.Length);
+                    if (reader <= 0)
+                    {
+                        break;
+                    }
+                    msreader.Write(buffer, 0, reader);
+                }
+                zip.Close();
+                ms.Close();
+                msreader.Position = 0;
+                buffer = msreader.ToArray();
+                msreader.Close();
+                return buffer;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
     }
 }
